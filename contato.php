e90841605d81ee6a6409107854a4cde432b619ad
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-10-12 20:18:13 -0300

    Upload img + contact.php + Style Contact

--- contato.php
+++ contato.php
@@ -1,7 +1,27 @@
 <?php
    include_once ('templates/header.php');
 ?>
-   <h1>Página de Contato</h1>
+   <div id="main-contact-container">
+      <div id="img-contact">
+         <img src="img/email.svg" alt="">
+      </div>
+      <div id="contact-container">
+         <h2>Entre em contato!</h2>
+         <p>Caso queira entrar em contato, mande uma mensagem!</p>
+         <form id="form-contact" action="#" method="POST">
+            <div>
+               <input type="text" name="nome" placeholder="Digite seu nome...">
+            </div>
+            <div>
+               <input id="email" type="email" name="email" placeholder="Digite seu e-mail...">
+            </div>
+            <div>
+               <textarea name="msg" placeholder="Sua mensagem..."></textarea>
+            </div>
+            <input class="btn-contact" type="submit" value="Enviar">
+         </form>
+      </div>
+   </div>
 <?php
    include_once ('templates/footer.php');
 ?>
