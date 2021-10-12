fba9bc4505d6f0dde08e3843d071ffaaa80f957b
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-10-12 01:42:06 -0300

    Upload index + contato

---
+++ contato.php
@@ -0,0 +1,7 @@
+<?php
+   include_once ('templates/header.php');
+?>
+   <h1>Página de Contato</h1>
+<?php
+   include_once ('templates/footer.php');
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+   include_once ('templates/header.php');
+?>
+   <h1>Olá mundo</h1>
+<?php
+   include_once ('templates/footer.php');
+?>
